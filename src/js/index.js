7621622486da3511a3780ecce3181ad591b20a42
Kennybabs-py <babalolakehinde74@gmail.com>
2024-11-21 00:39:17 +0100

    added lenis smooth scrolling

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,22 @@
+import Lenis from "lenis";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import { clock } from "./utils/clock";
+
+gsap.registerPlugin(ScrollTrigger);
+
+// ----------- Lenis -------------------//
+const lenis = new Lenis();
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
 
 setInterval(() => {
   clock();
